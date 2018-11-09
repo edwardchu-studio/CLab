0571539e21fc98b4ed85de338927077e7ab22670
Edward Chor <czy8023@me.com>
2018-11-09 00:24:28 -0500

    routine

---
+++ CPP/limits.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <climits>
+
+int limit()
+{
+  using namespace std;
+  int n_int=INT_MAX;
+  short n_short=SHRT_MAX;
+  cout<<n_int<<" "<<n_short<<endl;
+  return 0;
+}
+
--- CPP/main.cpp
+++ CPP/main.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 #include <cmath>
-// #include <string>
-
+#include <string>
+#include <climits>
+#include "limits.cpp"
 float mysqrt(int n)
 {
   return sqrt(n);
 }
+
 int main(void)
 {
   using namespace std;
-  int input;
-  cin>>input;
-  cout<<"The sqrt of "<<input<<" is "<<mysqrt(input)<<endl;
+  const int ArSize=20;
+  char name[ArSize];
+  char dessert[ArSize];
+  cout<<"Enter your name"<<endl;
+  cin.getline(name,ArSize);
+  cout<<"Enter your favorite dessert"<<endl;
+  cin.getline(dessert,ArSize);
+  cout<<"I have some delicious "<<dessert;
+  cout<<" for you, "<<name<<" .\n";
   return 0;
 }
+
